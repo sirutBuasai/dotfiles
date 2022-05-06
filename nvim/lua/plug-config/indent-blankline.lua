fca2ed84c3e9de04a100c2556fb253816b41fe97
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-05-06 00:25:37 -0400

    change indentation

--- nvim/lua/plug-config/indent-blankline.lua
+++ nvim/lua/plug-config/indent-blankline.lua
@@ -3,11 +3,56 @@ if not status_ok then
   return
 end
 
+vim.cmd [[highlight IndentBlanklineIndent1 guibg=None gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guibg=Gray15 gui=nocombine]]
+
 vim.opt.list = true
 vim.opt.listchars = "eol:¬"
+vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
+vim.g.indent_blankline_filetype_exclude = {
+  "help",
+  "dashboard",
+  "neogitstatus",
+  "NvimTree",
+  "Trouble",
+}
+vim.g.indentLine_enabled = 1
+vim.g.indent_blankline_show_trailing_blankline_indent = false
+vim.g.indent_blankline_show_first_indent_level = true
+vim.g.indent_blankline_use_treesitter = false
+vim.g.indent_blankline_show_current_context = true
+vim.g.indent_blankline_context_patterns = {
+  "class",
+  "return",
+  "function",
+  "method",
+  "^if",
+  "^while",
+  "jsx_element",
+  "^for",
+  "^object",
+  "^table",
+  "block",
+  "arguments",
+  "if_statement",
+  "else_clause",
+  "jsx_element",
+  "jsx_self_closing_element",
+  "try_statement",
+  "catch_clause",
+  "import_statement",
+  "operation_type",
+}
 
 indent_blankline.setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
+  char = "",
+    char_highlight_list = {
+      "IndentBlanklineIndent1",
+      "IndentBlanklineIndent2",
+    },
+    space_char_highlight_list = {
+      "IndentBlanklineIndent1",
+      "IndentBlanklineIndent2",
+    },
+    show_trailing_blankline_indent = false,
 }
