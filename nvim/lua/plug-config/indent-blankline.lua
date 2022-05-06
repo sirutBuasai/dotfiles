ea9adbe8c23a76867b9bd0206af59c3fb7bbd193
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-05-06 00:50:09 -0400

    format indent-blankline

--- nvim/lua/plug-config/indent-blankline.lua
+++ nvim/lua/plug-config/indent-blankline.lua
@@ -8,51 +8,53 @@ vim.cmd [[highlight IndentBlanklineIndent2 guibg=Gray15 gui=nocombine]]
 
 vim.opt.list = true
 vim.opt.listchars = "eol:¬"
-vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-vim.g.indent_blankline_filetype_exclude = {
-  "help",
-  "dashboard",
-  "neogitstatus",
-  "NvimTree",
-  "Trouble",
-}
-vim.g.indentLine_enabled = 1
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = true
-vim.g.indent_blankline_use_treesitter = false
-vim.g.indent_blankline_show_current_context = true
-vim.g.indent_blankline_context_patterns = {
-  "class",
-  "return",
-  "function",
-  "method",
-  "^if",
-  "^while",
-  "jsx_element",
-  "^for",
-  "^object",
-  "^table",
-  "block",
-  "arguments",
-  "if_statement",
-  "else_clause",
-  "jsx_element",
-  "jsx_self_closing_element",
-  "try_statement",
-  "catch_clause",
-  "import_statement",
-  "operation_type",
-}
 
 indent_blankline.setup {
   char = "",
-    char_highlight_list = {
-      "IndentBlanklineIndent1",
-      "IndentBlanklineIndent2",
-    },
-    space_char_highlight_list = {
-      "IndentBlanklineIndent1",
-      "IndentBlanklineIndent2",
-    },
-    show_trailing_blankline_indent = false,
+  char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+  },
+  space_char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+  },
+  buftype_exclude = {
+    "terminal",
+    "nofile",
+  },
+  indent_blankline_filetype_exclude = {
+    "help",
+    "dashboard",
+    "neogitstatus",
+    "NvimTree",
+    "Trouble",
+  },
+  indentLine_enabled = 1,
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = true,
+  use_treesitter = false,
+  show_current_context = true,
+  context_patterns = {
+    "class",
+    "return",
+    "function",
+    "method",
+    "^if",
+    "^while",
+    "jsx_element",
+    "^for",
+    "^object",
+    "^table",
+    "block",
+    "arguments",
+    "if_statement",
+    "else_clause",
+    "jsx_element",
+    "jsx_self_closing_element",
+    "try_statement",
+    "catch_clause",
+    "import_statement",
+    "operation_type",
+  },
 }
