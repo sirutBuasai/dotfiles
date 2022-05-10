ba16dd8b1f46eafd4295f311b16a594cebb28003
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-05-10 16:37:15 -0400

    add telescope cwd=./;
    git pom

--- nvim/lua/plug-config/telescope.lua
+++ nvim/lua/plug-config/telescope.lua
@@ -13,6 +13,7 @@ telescope.setup {
 }
 
 vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files cwd=..<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fc', ':Telescope find_files<CR>',        { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>',           { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>',         { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>',         { noremap = true })
