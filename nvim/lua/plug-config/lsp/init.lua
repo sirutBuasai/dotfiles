f58edf0457a8374ba6cfbe861bb5f494dd5015b6
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-05-26 22:46:06 -0700

    transfer from coc to native lsp

---
+++ nvim/lua/plug-config/lsp/init.lua
@@ -0,0 +1,8 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+require "plug-config.lsp.configs"
+require("plug-config.lsp.handlers").setup()
+require "plug-config.lsp.null-ls"
