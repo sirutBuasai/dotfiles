b42321f4d95bce0899703ac9af3ae563526b8804
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-08-01 10:33:14 -0700

    add neoscroll

--- nvim/lua/user/neoscroll.lua
+++ nvim/lua/user/neoscroll.lua
@@ -5,7 +5,7 @@ end
 
 neoscroll.setup {
     -- All these keys will be mapped to their corresponding default scrolling animation
-    mappings = {'<C-d>', '<C-u>'},
+    mappings = {'<C-d>', '<C-u>', '<C-y>', '<C-e>', 'zz', 'zt', 'zb'},
     hide_cursor = true,           -- Hide cursor while scrolling
     stop_eof = true,              -- Stop at <EOF> when scrolling downwards
     use_local_scrolloff = false,  -- Use the local scope of scrolloff instead of the global scope
