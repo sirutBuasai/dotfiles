b503f752313d4c6e39f390b15ce0d2b871b42691
Sirut Buasai <sirutbuasai27@hotmail.com>
2022-10-23 17:37:06 -0400

    change surround and illuminate

---
+++ nvim/lua/user/illuminate.lua
@@ -0,0 +1,23 @@
+local status_ok, illuminate = pcall(require, "illuminate")
+if not status_ok then
+  return
+end
+
+illuminate.configure({
+    providers = {
+        'lsp',
+        'treesitter',
+        'regex',
+    },
+    delay = 100,
+    filetype_overrides = {},
+    filetypes_denylist = {},
+    filetypes_allowlist = {},
+    modes_denylist = {},
+    modes_allowlist = {},
+    providers_regex_syntax_denylist = {},
+    providers_regex_syntax_allowlist = {},
+    under_cursor = true,
+    large_file_cutoff = nil,
+    large_file_overrides = nil,
+})
