388bde7a6da41e1d4d759e79780421dc6b67cfb5
Sirut Buasai <sirutbuasai27@hotmail.com>
2023-09-12 20:53:42 +0000

    add fsx temp for developement

--- zsh/zsh_custom_config/temp.sh
+++ zsh/zsh_custom_config/temp.sh
@@ -1,20 +1,20 @@
-alias mqp='cd ~/personal_dev/advanced_gen_modeling_mqp_2022_2023'
-alias zon='cd ~/workplace'
+alias zon='cd /fsx/workplace'
+alias p='cd /fsx/personal_dev'
 
-export PATH=$PATH:/Users/sbuasai/.toolbox/bin
-source /Users/sbuasai/.brazil_completion/zsh_completion
+# export PATH=$PATH:/Users/sbuasai/.toolbox/bin
+# source /Users/sbuasai/.brazil_completion/zsh_completion
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/sbuasai/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/sbuasai/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/sbuasai/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/Users/sbuasai/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
+# # >>> conda initialize >>>
+# # !! Contents within this block are managed by 'conda init' !!
+# __conda_setup="$('/Users/sbuasai/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+# if [ $? -eq 0 ]; then
+#     eval "$__conda_setup"
+# else
+#     if [ -f "/Users/sbuasai/anaconda3/etc/profile.d/conda.sh" ]; then
+#         . "/Users/sbuasai/anaconda3/etc/profile.d/conda.sh"
+#     else
+#         export PATH="/Users/sbuasai/anaconda3/bin:$PATH"
+#     fi
+# fi
+# unset __conda_setup
+# # <<< conda initialize <<<
