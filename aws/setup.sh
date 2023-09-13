b95ef9234fdf50ba44be14098ca98e0520c71038
Sirut Buasai <sirutbuasai27@hotmail.com>
2023-09-13 10:41:56 -0700

    add aws setup

---
+++ aws/setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# setup shell
+cd ~
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
+cp /fsx/personal_dev/dotfiles/aws/.bashrc ~
+
+# setup git
+cp /fsx/personal_dev/dotfiles/git/.gitconfig ~
+
+# setup neovim
+sudo snap install --beta nvim --classic
+sudo apt update
+sudo apt install nodejs
+sudo apt install npm
+mkdir -p ~/.config
+cp -r /fsx/personal_dev/dotfiles/nvim ~/.config/
+
+# setup tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp /fsx/personal_dev/dotfiles/tmux/.tmux.conf ~
