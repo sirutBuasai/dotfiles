606dd0427e5b69a23013677d8fc5e73b1b2597f3
Sirut Buasai <sirutbuasai27@hotmail.com>
2023-10-11 09:11:30 -0700

    remove conda init

--- zsh/zsh_custom_config/temp.sh
+++ zsh/zsh_custom_config/temp.sh
@@ -4,20 +4,6 @@ alias zon='cd ~/workplace'
 export PATH=$PATH:/Users/sbuasai/.toolbox/bin
 source /Users/sbuasai/.brazil_completion/zsh_completion
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/sbuasai/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/sbuasai/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/sbuasai/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/Users/sbuasai/anaconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
 function upaws(){
   if [[ $# -gt 0 ]]; then
     while [[ $# -gt 0 ]]; do
