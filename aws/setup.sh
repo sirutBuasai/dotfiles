b3fab3a6e4184230ab75ad0dda229af7d494fc75
sirutBuasai <sirutbuasai27@outlook.com>
2025-05-01 14:39:47 -0700

    fix pathing

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # setup shell
-cd ~
+cd $HOME
 bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)"
-cp -f aws/.bashrc $HOME
+cp -f $HOME/dotfiles/aws/.bashrc $HOME
 
 # setup git
-cp -f git/.gitconfig $HOME
+cp -f $HOME/dotfiles/git/.gitconfig $HOME
 
 # setup tmux
 sudo apt install tmux
 rm -rf $HOME/tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm $HOME/tmux/plugins/tpm
-cp -f tmux/.tmux.conf $HOME
+cp -f $HOME/dotfiles/tmux/.tmux.conf $HOME
 
 # setup conda/mamba
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
@@ -24,4 +24,4 @@ sudo apt update
 sudo apt install nodejs
 sudo apt install npm
 mkdir -p $HOME/config
-cp -rf nvim $HOME/config/
+cp -rf $HOME/dotfiles/nvim $HOME/config/
